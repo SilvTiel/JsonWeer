2308e1982a03fadc9712923ea0e200b021c32e8e
Sil van Tiel <silvantiel@gmail.com>
2021-06-02 16:44:24 +0200

    Funcie van weer toegevoegd, Tempratuur, dorp.

--- jsonweer.js
+++ jsonweer.js
@@ -1,3 +1,9 @@
-fetch('https://weerlive.nl/api/json-data-10min.php?key=7964ce7459&locatie=Erp')
+fetch('https://weerlive.nl/api/json-data-10min.php?key=7964ce7459&locatie=Veghel')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => weer(data))
+
+  function weer(data){
+    document.getElementById("name").innerHTML = data.liveweer[0].plaats
+    document.getElementById("temp").innerHTML = data.liveweer[0].temp
+    document.getElementById("desc").innerHTML = data.liveweer[0].verw
+  }
